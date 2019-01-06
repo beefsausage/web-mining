314f510d0a2f00b2e264ebd114324d9a45fc3407
Andre Kastner <andre@akastner.de>
2019-01-07 00:37:25 +0100

    reworked mapping

--- miner.py
+++ miner.py
@@ -15,19 +15,32 @@ replacers = {
 "language" : "",
 " later " : " ",
 "-" : " ",
+"_" : " ",
 "(" : "",
-")" : ""
+")" : "",
 }
 repRegexPattern = re.compile("(%s)" % "|".join(map(re.escape, replacers.keys())))
 
 def normalize(str):
-    return repRegexPattern.sub(lambda x: replacers[x.string[x.start():x.end()]], str).lower()
+    return repRegexPattern.sub(lambda x: replacers[x.string[x.start():x.end()]], str).strip().lower()
 
 def split(str):
-    return [x.strip() for x in re.split(delRegexPattern, normalize(str))]
+    return [x.strip() for x in re.split(delRegexPattern, str)]
 
 def similar(str1, str2):
-    return SequenceMatcher(None, str1, str2).ratio()
+    return SequenceMatcher(None, str1, str2).quick_ratio()
+
+def occurrenceCounter(dic, str):
+    if str in dic:
+        dic[str] += 1
+    else:
+        dic[str] = 1
+
+def appendToDicValue(dic, key, value):
+    if key in dic:
+        dic[key].append(value)
+    else:
+        dic[key] = [value]
 
 sparql = SPARQLWrapper("http://dbpedia.org/sparql")
 
@@ -48,18 +61,17 @@ sparql.setQuery("""
 """)
 
 results = sparql.query().convert()
-print(len(results['results']['bindings']))
 queried = {}
-paradigmas = {}
+paradigmaOccurences = {}
+languages = {}
 renderSet = []
-paradigmCount = {}
-notmatched = {}
 
 g = Graph(filename='miner.gv', engine='sfdp', format='png')
 g.attr(size='20', repulsiveforce='0.1', overlap='false',splines='curved')
 for result in results["results"]["bindings"]:
     name = result["pl"]["value"].split("resource/")
     paradigm = result["paradigm"]["value"]
+
     if paradigm.startswith("http://dbpedia.org"):
         if paradigm not in queried:
             queryParadigm = """
@@ -75,21 +87,22 @@ for result in results["results"]["bindings"]:
                 queried[paradigm] = paradigmLabel["results"]["bindings"][0]["label"]["value"]
             else:
                 queried[paradigm] = paradigm.split("resource/")[1]
-        print('%s: %s' % (name[1], queried.get(paradigm)))
-        paradigmas[name[1]] = queried.get(paradigm)
-        renderSet.append((queried.get(paradigm).replace("programming ","").replace(" programming", ""),name[1]))
-    else:
-        print('%s: %s' % (name[1], result["paradigm"]["value"]))
-        notmatched[name[1]] = result["paradigm"]["value"]
 
-for p in notmatched:
-    paradigmList = split(notmatched[p])
-    for elem in paradigmList:
-        for k,v in queried.items():
-            similarity = similar(normalize(elem), normalize(v))
-            if similarity > 0.95:
-                g.edge(v, p)
-                print('%s,%s => %s, %s' % (p, elem, v, similarity))
+        #print('%s: %s' % (name[1], queried.get(paradigm)))
+        occurrenceCounter(paradigmaOccurences, queried.get(paradigm))
+        appendToDicValue(languages, name[1], queried.get(paradigm))
+
+    else:
+        #print('%s: %s' % (name[1], result["paradigm"]["value"]))
+        languages[name[1]] = split(result["paradigm"]["value"])
+
+for language, paradigmlist in languages.items():
+    for e in paradigmlist:
+        for pname, count in sorted(paradigmaOccurences.items(), key=lambda x: x[1], reverse = True):
+            s = similar(normalize(e),normalize(pname))
+            if s > 0.95:
+                print('%s: %s => %s: %s %%' % (language, e, pname,s*100))
+                renderSet.append((pname,language))
                 break
 
 g.node_attr.update(color='#9dd600', style='filled', fontname='helvetica')
@@ -109,8 +122,8 @@ g.attr('node',
     width="2", height="2")
 for renderEntry in renderSet:
     g.edge(renderEntry[0], renderEntry[1])
+    #print(renderEntry)
 
 print(len(renderSet))
-
 g.render()
 
